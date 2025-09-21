0299dd5a58041734af9d5780bf6bd07c8322fa73
Yukina0228 <1823473758@qq.com>
2025-09-21 18:01:42 +0800

    7. 工程项目构建

--- src/main/java/com/yukina/suaicode/core/handler/JsonMessageStreamHandler.java
+++ src/main/java/com/yukina/suaicode/core/handler/JsonMessageStreamHandler.java
@@ -5,9 +5,12 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.yukina.suaicode.ai.model.message.*;
+import com.yukina.suaicode.constant.AppConstant;
+import com.yukina.suaicode.core.builder.VueProjectBuilder;
 import com.yukina.suaicode.model.entity.User;
 import com.yukina.suaicode.model.enums.ChatHistoryMessageTypeEnum;
 import com.yukina.suaicode.service.ChatHistoryService;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -22,6 +25,9 @@ import java.util.Set;
 @Slf4j
 @Component
 public class JsonMessageStreamHandler {
+
+    @Resource
+    private VueProjectBuilder vueProjectBuilder;
 
     /**
      * 处理 TokenStream（VUE_PROJECT）
@@ -50,6 +56,9 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatHistory(aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), appId, loginUser.getId());
+                    // 异步构建vue项目
+                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue-project_" + appId;
+                    vueProjectBuilder.buildProjectAsync(projectPath);
                 })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息
